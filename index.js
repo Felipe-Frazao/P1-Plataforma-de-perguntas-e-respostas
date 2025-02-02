f4e1dd63efc87f1e111e590ab35e03d34a434fef
Felipe Frazão <felipe.frazao21@gmail.com>
2025-02-02 20:42:05 -0300

    commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
-const app = express()
-
+const app = express();
+const bodyParser = require("body-parser"); //responsavel pela conversao dos dados enviados no formulario para JS
 
 //informando ao express que e para utilizar o EJS como principal view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public')); //Arquivos staticos
+
+app.use(bodyParser.urlencoded({extended: false})); //decodificar os dados enviados pelo formulario
+app.use(bodyParser.json());//Permiti a leitura de dados enviados via json utilizado com API
 
 app.get("/", (req,res) => {
     res.render("index");
@@ -14,8 +17,15 @@ app.get("/perguntar", (req,res) => {
     res.render("perguntar");
 });
 
+app.post("/salvarpergunta",(req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
+});
+
 app.listen(8080, ()=> {
     console.log("servidor conectado!!")
 });
+
 
 //include("partials/header.ejs") nova maneira de uso do include para reutilizacao de codigo
